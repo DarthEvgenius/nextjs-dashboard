fefd811d3ddc45ae6cea73763d0335ce3650cb8c
Eugene Zinin <e.zinin@joki-joya.ru>
2025-08-14 18:04:51 +0300

    zod form validation for create form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,21 +9,50 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+    message: 'Please select a customer',
+  }),
+
+  amount: z.coerce
+    .number()
+    .gt(0, {
+      message: 'Please enter an amount greater than 0'
+    }),
+  
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+    message: 'Please select an invoice status'
+  }),
   date: z.string(),
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  }
+  message?: string | null;
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
   const rawFormData = Object.fromEntries(formData.entries())
   console.log('rawData:', rawFormData) // in terminal!
 
-  const { customerId, amount, status } = CreateInvoice.parse(rawFormData)
+  const validatedFields = CreateInvoice.safeParse(rawFormData)
+  
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice'
+    }
+  }
 
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
 
@@ -33,7 +62,9 @@ export async function createInvoice(formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `
   } catch (e) {
-    console.log('error:', e)
+    return {
+      message: 'Database Error: Failed to Create Invoice',
+    };
   }
 
   revalidatePath('/dashboard/invoices')
